ebd9938728c616a5f27ab8eb3492ae0cac5ab52c
Szentes Szabolcs <szmizorsz@gmail.com>
2023-03-22 11:14:11 +0100

    Displaying shorter version of the ID

--- packages/ui/components/firms.tsx
+++ packages/ui/components/firms.tsx
@@ -1,12 +1,25 @@
 import React from "react";
-import { Table, Thead, Tbody, Tr, Th, Td, Spinner } from "@chakra-ui/react";
-import { useCompanies } from "@/hooks/useCompanies";
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  Spinner,
+  Text,
+  Tooltip,
+} from "@chakra-ui/react";
+import { useCompanies, EnhancedFirmsQueryQuery } from "@/hooks/useCompanies";
 
 function Firms() {
   const { data, isLoading } = useCompanies();
 
   return (
     <>
+      <Text fontSize="2xl" mb={4}>
+        Internet-native companies
+      </Text>
       {isLoading ? (
         <Spinner />
       ) : (
@@ -20,14 +33,20 @@ function Firms() {
             </Tr>
           </Thead>
           <Tbody>
-            {data?.newFirmCreateds.map((firm) => (
-              <Tr key={firm.id}>
-                <Td>{firm.id}</Td>
-                <Td>{firm.creator}</Td>
-                <Td>{firm.safe}</Td>
-                <Td>{firm.blockNumber}</Td>
-              </Tr>
-            ))}
+            {data?.newFirmCreateds.map(
+              (firm: EnhancedFirmsQueryQuery["newFirmCreateds"][number]) => (
+                <Tr key={firm.id}>
+                  <Td>
+                    <Tooltip label={firm.id} fontSize="md">
+                      {firm.idDisplay}
+                    </Tooltip>
+                  </Td>
+                  <Td>{firm.creator}</Td>
+                  <Td>{firm.safe}</Td>
+                  <Td>{firm.blockNumber}</Td>
+                </Tr>
+              )
+            )}
           </Tbody>
         </Table>
       )}
--- packages/ui/hooks/useCompanies.ts
+++ packages/ui/hooks/useCompanies.ts
@@ -1,13 +1,36 @@
 import React, { useEffect } from "react";
-import { FirmsQueryDocument, FirmsQueryQuery, execute } from "../.graphclient";
+import {
+  FirmsQueryDocument,
+  FirmsQueryQuery,
+  execute,
+  NewFirmCreated,
+} from "../.graphclient";
+
+export type EnhancedFirmsQueryQuery = FirmsQueryQuery & {
+  newFirmCreateds: Array<
+    Pick<NewFirmCreated, "id" | "creator" | "safe" | "blockNumber"> & {
+      idDisplay: string;
+    }
+  >;
+};
 
 export function useCompanies() {
-  const [data, setData] = React.useState<FirmsQueryQuery>();
+  const [data, setData] = React.useState<EnhancedFirmsQueryQuery>();
   const [isLoading, setIsLoading] = React.useState<boolean>(true);
 
   useEffect(() => {
     execute(FirmsQueryDocument, {}).then((result) => {
-      setData(result?.data);
+      const enhancedResult = result?.data as EnhancedFirmsQueryQuery;
+      if (enhancedResult?.newFirmCreateds) {
+        enhancedResult.newFirmCreateds = enhancedResult.newFirmCreateds.map(
+          (firm) => ({
+            ...firm,
+            idDisplay: `${firm.id.substr(0, 6)}...${firm.id.substr(-8)}`,
+          })
+        );
+      }
+      setData(enhancedResult);
+      debugger;
       setIsLoading(false);
     });
   }, [setData]);
